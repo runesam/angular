6be488802d7d0f8ab51cf39abefde8f9efea1c7b
runesam <same7.hamada@gmail.com>
2019-10-09 08:06:01 +0200

    implement outputting via event emitters

--- introApp/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ introApp/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 
@@ -8,18 +8,53 @@ import { Passenger } from '../../models/passenger.interface';
   template: `
     <div>
       <span class="status" [class.checked-in]="detail.checkedIn"></span>
-      {{detail.fullName}}
+      <div *ngIf="editing">
+        <input
+          #name
+          type="text"
+          [value]="detail.fullName"
+          (input)="onNameChange(name.value)"
+        >
+      </div>
+      <div *ngIf="!editing">
+        {{detail.fullName}}
+      </div>
       <div class="date">
         check in date:
-        {{detail.checkInDate ? (detail.checkInDate | date: 'MMMM dd, yyyy') : 'Not checked in' }}
+        {{detail.checkInDate ? (detail.checkInDate | date: 'MMMM dd, yyyy') : 'Not checked in'}}
       </div>
       <div class="children">
         Children: {{detail.children?.length || 0}}
       </div>
+      <button (click)="toggleEdit()">{{editing ? 'Save' : 'Edit'}}</button>
+      <button (click)="onRemove()">Remove</button>
     </div>
   `,
 })
 export class PassengerDetailComponent {
   @Input()
   detail: Passenger;
+
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  editing: boolean = false;
+
+  onNameChange = (fullName: string) => {
+    this.detail.fullName = fullName;
+  };
+
+  toggleEdit = () => {
+    if (this.editing) {
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  };
+
+  onRemove = () => {
+    this.remove.emit(this.detail);
+  };
 }
